e514225c2ef8b41d5c0d9b9f59e8935251f13ebf
wakenedo <a_nuner@outlook.com>
2025-03-15 20:06:34 -0300

    feat:InterfaceSwitch initial implementation

--- RecoletaNative/app/(tabs)/index.tsx
+++ RecoletaNative/app/(tabs)/index.tsx
@@ -1,13 +1,13 @@
-import { AuthProvider, useAuth } from '../context/AuthContext';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Layout } from './_layout';
+import { AuthProvider, useAuth } from "../context/AuthContext";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Layout } from "./_layout";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-    return(
-        <AuthProvider>
-            <Layout></Layout>
-        </AuthProvider>
-    );
+  return (
+    <AuthProvider>
+      <Layout />
+    </AuthProvider>
+  );
 }
