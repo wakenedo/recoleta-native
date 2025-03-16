5c22d8336f8cba71d9f8fed403e428ffe4a99654
Luizustavo <luizg.ss@outlook.com>
2025-03-15 22:20:11 -0300

    add Select component with styling and actionsheet integration; update package dependencies

--- RecoletaNative/app/(tabs)/Login.tsx
+++ RecoletaNative/app/(tabs)/Login.tsx
@@ -9,13 +9,34 @@ import { Heading } from "@/components/ui/heading";
 import { Input, InputField, InputIcon, InputSlot } from "@/components/ui/input";
 import { Text } from "@/components/ui/text";
 import { VStack } from "@/components/ui/vstack";
-import { EyeIcon, EyeOffIcon, MailIcon, LockIcon } from "@/components/ui/icon";
+
+import {
+  EyeIcon,
+  EyeOffIcon,
+  MailIcon,
+  LockIcon,
+  ChevronDownIcon,
+  User,
+} from "lucide-react-native";
 import { Image } from "@/components/ui/image";
+import {
+  Select,
+  SelectBackdrop,
+  SelectContent,
+  SelectIcon,
+  SelectInput,
+  SelectItem,
+  SelectPortal,
+  SelectTrigger,
+} from "@/components/ui/select";
 
 // Schema de validação
 const loginSchema = z.object({
   email: z.string().email("Email inválido"),
   password: z.string().min(6, "Senha deve ter pelo menos 6 caracteres"),
+  firstName: z.string().optional(),
+  lastName: z.string().optional(),
+  userType: z.string().optional(),
 });
 
 type FormData = z.infer<typeof loginSchema>;
@@ -76,66 +97,210 @@ const AuthScreen = () => {
         </Text>
 
         <VStack space="md">
-          <Controller
-            control={control}
-            name="email"
-            render={({ field }) => (
-              <VStack space="xs">
-                <Text>Email</Text>
-                <Input
-                  isInvalid={!!errors.email}
-                  className="min-w-[250px] border border-transparent rounded-md bg-zinc-100 h-12 "
-                >
-                  <InputIcon as={MailIcon} className="ml-2 text-zinc-400" />
-                  <InputField
-                    placeholder="email@exemplo.com"
-                    value={field.value}
-                    onChangeText={field.onChange}
-                    keyboardType="email-address"
-                    autoCapitalize="none"
-                  />
-                </Input>
-                {errors.email && (
-                  <Text className="text-red-500 text-sm">
-                    {errors.email.message}
-                  </Text>
-                )}
-              </VStack>
-            )}
-          />
+          {isRegistering ? (
+            <>
+              <Controller
+                control={control}
+                name="firstName"
+                render={({ field }) => (
+                  <VStack space="xs">
+                    <Text>Nome completo</Text>
+                    <Input
+                      isInvalid={!!errors.firstName}
+                      className="min-w-[250px] border border-transparent rounded-md bg-zinc-100 h-12"
+                    >
+                      <User className="ml-2 text-zinc-400" />
 
-          <Controller
-            control={control}
-            name="password"
-            render={({ field }) => (
-              <VStack space="xs">
-                <Text>Senha</Text>
-                <Input
-                  isInvalid={!!errors.password}
-                  className="min-w-[250px] border border-transparent rounded-md bg-zinc-100 h-12"
-                >
-                  <InputIcon as={LockIcon} className="ml-2 text-zinc-400" />
-                  <InputField
-                    placeholder="Informe sua senha"
-                    value={field.value}
-                    onChangeText={field.onChange}
-                    secureTextEntry={!showPassword}
-                  />
-                  <InputSlot onPress={() => setShowPassword(!showPassword)}>
-                    <InputIcon
-                      as={showPassword ? EyeIcon : EyeOffIcon}
-                      className="text-zinc-400 mr-2"
-                    />
-                  </InputSlot>
-                </Input>
-                {errors.password && (
-                  <Text className="text-red-500 text-sm">
-                    {errors.password.message}
-                  </Text>
+                      <InputField
+                        placeholder="Informe seu nome completo"
+                        value={field.value}
+                        onChangeText={field.onChange}
+                        autoCapitalize="none"
+                      />
+                    </Input>
+                    {errors.firstName && (
+                      <Text className="text-red-500 text-sm">
+                        {errors.firstName.message}
+                      </Text>
+                    )}
+                  </VStack>
                 )}
-              </VStack>
-            )}
-          />
+              />
+              <Controller
+                control={control}
+                name="email"
+                render={({ field }) => (
+                  <VStack space="xs">
+                    <Text>Email</Text>
+                    <Input
+                      isInvalid={!!errors.email}
+                      className="min-w-[250px] border border-transparent rounded-md bg-zinc-100 h-12"
+                    >
+                      <MailIcon className="ml-2 text-zinc-400" />
+                      <InputField
+                        placeholder="email@exemplo.com"
+                        value={field.value}
+                        onChangeText={field.onChange}
+                        keyboardType="email-address"
+                        autoCapitalize="none"
+                      />
+                    </Input>
+                    {errors.email && (
+                      <Text className="text-red-500 text-sm">
+                        {errors.email.message}
+                      </Text>
+                    )}
+                  </VStack>
+                )}
+              />
+              <Controller
+                control={control}
+                name="password"
+                render={({ field }) => (
+                  <VStack space="xs">
+                    <Text>Senha</Text>
+                    <Input
+                      isInvalid={!!errors.password}
+                      className="min-w-[250px] border border-transparent rounded-md bg-zinc-100 h-12"
+                    >
+                      <LockIcon className="ml-2 text-zinc-400" />
+                      <InputField
+                        placeholder="Informe sua senha"
+                        value={field.value}
+                        onChangeText={field.onChange}
+                        secureTextEntry={!showPassword}
+                      />
+                      <InputSlot onPress={() => setShowPassword(!showPassword)}>
+                        <InputIcon
+                          as={showPassword ? EyeIcon : EyeOffIcon}
+                          className="text-zinc-400 mr-2"
+                        />
+                      </InputSlot>
+                    </Input>
+                    {errors.password && (
+                      <Text className="text-red-500 text-sm">
+                        {errors.password.message}
+                      </Text>
+                    )}
+                  </VStack>
+                )}
+              />
+              <Controller
+                control={control}
+                name="userType"
+                render={({ field }) => (
+                  <VStack space="xs">
+                    <Text>Tipo de Usuário</Text>
+                    <Select
+                      onValueChange={field.onChange}
+                      selectedValue={field.value}
+                      isDisabled={isLoading}
+                    >
+                      <SelectTrigger className="w-full bg-zinc-100 h-12 rounded-md border-0">
+                        <SelectInput
+                          placeholder="Selecione o tipo"
+                          placeholderTextColor="#9CA3AF"
+                          className="text-black pl-3 text-base"
+                          pointerEvents="none"
+                        />
+                        <SelectIcon className="mr-3">
+                          <ChevronDownIcon size={20} color="#6B7280" />
+                        </SelectIcon>
+                      </SelectTrigger>
+
+                      <SelectPortal>
+                        <SelectBackdrop />
+                        <SelectContent className="w-full bg-white rounded-lg shadow-lg">
+                          <SelectItem
+                            label="Pessoa Física"
+                            value="INDIVIDUAL"
+                            className="px-4 py-3 border-b border-gray-200"
+                            /*                             textClassName="text-black"
+                             */
+                          />
+                          <SelectItem
+                            label="Organização"
+                            value="ORGANIZATION"
+                            className="px-4 py-3"
+                            /*                             textClassName="text-black"
+                             */
+                          />
+                        </SelectContent>
+                      </SelectPortal>
+                    </Select>
+                    {errors.userType && (
+                      <Text className="text-red-500 text-sm mt-1">
+                        {errors.userType.message}
+                      </Text>
+                    )}
+                  </VStack>
+                )}
+              />
+            </>
+          ) : (
+            <>
+              {" "}
+              <Controller
+                control={control}
+                name="email"
+                render={({ field }) => (
+                  <VStack space="xs">
+                    <Text>Email</Text>
+                    <Input
+                      isInvalid={!!errors.email}
+                      className="min-w-[250px] border border-transparent rounded-md bg-zinc-100 h-12 "
+                    >
+                      <MailIcon className="ml-2 text-zinc-400" />
+                      <InputField
+                        placeholder="email@exemplo.com"
+                        value={field.value}
+                        onChangeText={field.onChange}
+                        keyboardType="email-address"
+                        autoCapitalize="none"
+                      />
+                    </Input>
+                    {errors.email && (
+                      <Text className="text-red-500 text-sm">
+                        {errors.email.message}
+                      </Text>
+                    )}
+                  </VStack>
+                )}
+              />
+              <Controller
+                control={control}
+                name="password"
+                render={({ field }) => (
+                  <VStack space="xs">
+                    <Text>Senha</Text>
+                    <Input
+                      isInvalid={!!errors.password}
+                      className="min-w-[250px] border border-transparent rounded-md bg-zinc-100 h-12"
+                    >
+                      <LockIcon className="ml-2 text-zinc-400" />
+                      <InputField
+                        placeholder="Informe sua senha"
+                        value={field.value}
+                        onChangeText={field.onChange}
+                        secureTextEntry={!showPassword}
+                      />
+                      <InputSlot onPress={() => setShowPassword(!showPassword)}>
+                        <InputIcon
+                          as={showPassword ? EyeIcon : EyeOffIcon}
+                          className="text-zinc-400 mr-2"
+                        />
+                      </InputSlot>
+                    </Input>
+                    {errors.password && (
+                      <Text className="text-red-500 text-sm">
+                        {errors.password.message}
+                      </Text>
+                    )}
+                  </VStack>
+                )}
+              />
+            </>
+          )}
 
           <Button
             onPress={handleSubmit(handleAuth)}
