afe746ee29e72fd2f030bfdf1193583a6d3dd4ca
Luizustavo <luizg.ss@outlook.com>
2025-03-25 01:06:08 -0300

    feat: add Toast component and update package dependencies

--- RecoletaNative/app/(tabs)/index.tsx
+++ RecoletaNative/app/(tabs)/index.tsx
@@ -1,6 +1,8 @@
+import React from "react";
 import { AuthProvider, useAuth } from "../context/AuthContext";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Layout } from "./_layout";
+import Toast from "react-native-toast-message";
 
 const Stack = createNativeStackNavigator();
 
@@ -8,6 +10,7 @@ export default function App() {
   return (
     <AuthProvider>
       <Layout />
+      <Toast />
     </AuthProvider>
   );
 }
