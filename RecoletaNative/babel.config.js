f1e700716d1cbc641a88453a54fe23d01f97ed27
Luizustavo <luizg.ss@outlook.com>
2025-03-25 00:14:59 -0300

    refactor: update metro and babel configurations for improved compatibility

--- RecoletaNative/babel.config.js
+++ RecoletaNative/babel.config.js
@@ -1,29 +1,18 @@
-module.exports = function (api) {
+module.exports = function(api) {
   api.cache(true);
 
   return {
-    presets: [
-      [
-        "babel-preset-expo",
-        {
-          jsxImportSource: "nativewind",
-        },
-      ],
-      "nativewind/babel",
-    ],
+      presets: [["babel-preset-expo", {
+          jsxImportSource: "nativewind"
+      }], "nativewind/babel"],
 
-    plugins: [
-      [
-        "module-resolver",
-        {
+      plugins: [["module-resolver", {
           root: ["./"],
+
           alias: {
-            "@": "./",
-            "tailwind.config": "./tailwind.config.js",
-          },
-        },
-      ],
-      "react-native-reanimated/plugin",
-    ],
+              "@": "./",
+              "tailwind.config": "./tailwind.config.js"
+          }
+      }]]
   };
 };
